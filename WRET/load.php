36d29884f72374ea04ac72afe1ae6bf56bb1b829
Charlotte Herice <cherice@emi.u-bordeaux1.fr>
2013-02-13 10:56:18 +0100

    load et options w3c

--- WRET/load.php
+++ WRET/load.php
@@ -22,7 +22,6 @@
 		$de=$currentUrl."lang=de";
 	?>
 	<div id="menu">
-		
 		<ul>
 			<li><a href="index.php">		<?php echo TXT_MENU_HOME; ?>	</a></li>
 			<li><a href="create.php">		<?php echo TXT_MENU_CREATE; ?> 	</a></li>
@@ -60,20 +59,20 @@
 					</div>
 					<div id="buttons">
 						<div id="buttons3-1">
-							<input type="radio" name="choix1" 	value="log console" checked="checked">	<?php echo TXT_OPTIONS_SAVING_3; ?> </br>
-							<input type="radio" name="choix1" 	value="log file"> 						<?php echo TXT_OPTIONS_SAVING_4; ?> </br>
+							<input type="radio" name="choix1" 	value="log console" checked="checked">	<?php echo TXT_OPTIONS_SAVING_3; ?> <br/>
+							<input type="radio" name="choix1" 	value="log file"> 						<?php echo TXT_OPTIONS_SAVING_4; ?> <br/>
 							<?php echo TXT_OPTIONS_SAVING_5; ?> 	<input type="text"	   name="log_nomFichier" 	size="10" id="texte1">
 						</div>
 						<div id="buttons3-2">
-							<input type="radio" name="choix2" value="level WARNING"> 	<?php echo TXT_OPTIONS_ERROR_2; ?> </br>
-					 		<input type="radio" name="choix2" value="level CONFIG"> 	<?php echo TXT_OPTIONS_ERROR_3; ?> </br>
-					 		<input type="radio" name="choix2" value="level INFO"> 		<?php echo TXT_OPTIONS_ERROR_4; ?> </br>
-					 		<input type="radio" name="choix2" value="level FINE"> 		<?php echo TXT_OPTIONS_ERROR_5; ?> </br>
-					 		<input type="radio" name="choix2" value="level FINER"> 		<?php echo TXT_OPTIONS_ERROR_6; ?> </br>
+							<input type="radio" name="choix2" value="level WARNING"> 	<?php echo TXT_OPTIONS_ERROR_2; ?> <br/>
+					 		<input type="radio" name="choix2" value="level CONFIG"> 	<?php echo TXT_OPTIONS_ERROR_3; ?> <br/>
+					 		<input type="radio" name="choix2" value="level INFO"> 		<?php echo TXT_OPTIONS_ERROR_4; ?> <br/>
+					 		<input type="radio" name="choix2" value="level FINE"> 		<?php echo TXT_OPTIONS_ERROR_5; ?> <br/>
+					 		<input type="radio" name="choix2" value="level FINER"> 		<?php echo TXT_OPTIONS_ERROR_6; ?> <br/>
 					 		<input type="radio" name="choix2" value="level FINEST" checked="checked"> 	<?php echo TXT_OPTIONS_ERROR_7; ?> 	
 						</div>
 						<div id="buttons3-3">
-							<input type="radio" name="choix3" 	value="format default"> 					<?php echo TXT_OPTIONS_SAVING_7; ?>	</br>
+							<input type="radio" name="choix3" 	value="format default"> 					<?php echo TXT_OPTIONS_SAVING_7; ?>	<br/>
 							<input type="radio" name="choix3" 	value="format plain" checked="checked"> 	<?php echo TXT_OPTIONS_SAVING_8; ?>	
 						</div>
 					</div>
@@ -92,11 +91,11 @@
 					</div>
 					<div id="buttons">
 						<div id="buttons2-1">
-							<input type="radio" name="choix4" value="kind flux-analyzer-dir"> 		<?php echo TXT_OPTIONS_INPUT_STOECH_1; ?>	</br>
-						 	<input type="radio" name="choix4" value="kind flux-analyzer-files"> 	<?php echo TXT_OPTIONS_INPUT_STOECH_2; ?>	</br>
-							<input type="radio" name="choix4" value="kind reaction-list"> 			<?php echo TXT_OPTIONS_INPUT_STOECH_3; ?> 	</br>
-						 	<input type="radio" name="choix4" value="kind excel"> 					<?php echo TXT_OPTIONS_INPUT_STOECH_4; ?>	</br>
-						 	<input type="radio" name="choix4" value="kind stoichiometry" checked="checked"> 			<?php echo TXT_OPTIONS_INPUT_STOECH_5; ?>	</br>
+							<input type="radio" name="choix4" value="kind flux-analyzer-dir"> 		<?php echo TXT_OPTIONS_INPUT_STOECH_1; ?>	<br/>
+						 	<input type="radio" name="choix4" value="kind flux-analyzer-files"> 	<?php echo TXT_OPTIONS_INPUT_STOECH_2; ?>	<br/>
+							<input type="radio" name="choix4" value="kind reaction-list"> 			<?php echo TXT_OPTIONS_INPUT_STOECH_3; ?> 	<br/>
+						 	<input type="radio" name="choix4" value="kind excel"> 					<?php echo TXT_OPTIONS_INPUT_STOECH_4; ?>	<br/>
+						 	<input type="radio" name="choix4" value="kind stoichiometry" checked="checked"> 			<?php echo TXT_OPTIONS_INPUT_STOECH_5; ?>	<br/>
 						 	<input type="radio" name="choix4" value="kind sbml"> 					<?php echo TXT_OPTIONS_INPUT_STOECH_6; ?>				
 						</div>
 						<div id="buttons2-2">
@@ -122,11 +121,11 @@
 					</div>
 					<div id="buttons">
 						<div id="buttons1">
-							<input type="radio" name="choix6" value="compression default"> 					<?php echo TXT_OPTIONS_COMPRESS_1; ?>	 </br>
-							<input type="radio" name="choix6" value="compression off"> 						<?php echo TXT_OPTIONS_COMPRESS_2; ?>	 </br>
-							<input type="radio" name="choix6" value="compression unique"> 					<?php echo TXT_OPTIONS_COMPRESS_3; ?>	 </br>
-							<input type="radio" name="choix6" value="compression nullspace"> 				<?php echo TXT_OPTIONS_COMPRESS_4; ?>	 </br>
-							<input type="radio" name="choix6" value="compression unique-no-recursion"> 		<?php echo TXT_OPTIONS_COMPRESS_5; ?> 	 </br>
+							<input type="radio" name="choix6" value="compression default"> 					<?php echo TXT_OPTIONS_COMPRESS_1; ?>	 <br/>
+							<input type="radio" name="choix6" value="compression off"> 						<?php echo TXT_OPTIONS_COMPRESS_2; ?>	 <br/>
+							<input type="radio" name="choix6" value="compression unique"> 					<?php echo TXT_OPTIONS_COMPRESS_3; ?>	 <br/>
+							<input type="radio" name="choix6" value="compression nullspace"> 				<?php echo TXT_OPTIONS_COMPRESS_4; ?>	 <br/>
+							<input type="radio" name="choix6" value="compression unique-no-recursion"> 		<?php echo TXT_OPTIONS_COMPRESS_5; ?> 	 <br/>
 							<input type="radio" name="choix6" value="compression nullspace-no-recursion"> 	<?php echo TXT_OPTIONS_COMPRESS_6; ?>
 						</div>
 					</div>
@@ -137,22 +136,22 @@
 					</div>
 					<div id="buttons">
 						<div id="buttons1">
-							<input type="radio" 	name="choix7" 	value="out null"> 			<?php echo TXT_OPTIONS_OUTPUT_1; ?> 			</br>
-							<input type="radio"		name="choix7" 	value="out count"> 			<?php echo TXT_OPTIONS_OUTPUT_2; ?>	 			</br>
+							<input type="radio" 	name="choix7" 	value="out null"> 			<?php echo TXT_OPTIONS_OUTPUT_1; ?> 			<br/>
+							<input type="radio"		name="choix7" 	value="out count"> 			<?php echo TXT_OPTIONS_OUTPUT_2; ?>	 			<br/>
 							<input type="radio" 	name="choix7" 	value="out text-boolean"> 	<?php echo TXT_OPTIONS_OUTPUT_3; ?>
-							<?php echo TXT_OPTIONS_OUTPUT_4; ?> 	<input 	type="text" 		name="out_nomFichier" 	size="10" id="texte7"> 	</br>
+							<?php echo TXT_OPTIONS_OUTPUT_4; ?> 	<input 	type="text" 		name="out_nomFichier" 	size="10" id="texte7"> 	<br/>
 							<input type="radio" 	name="choix7" 	value="out text-doubles"> 	<?php echo TXT_OPTIONS_OUTPUT_5; ?>
-							<?php echo TXT_OPTIONS_OUTPUT_6; ?> 	<input 	type="text" 		name="out_nomFichier" 	size="10" id="texte71"> </br>	
+							<?php echo TXT_OPTIONS_OUTPUT_6; ?> 	<input 	type="text" 		name="out_nomFichier" 	size="10" id="texte71"> <br/>	
 							<input type="radio" 	name="choix7" 	value="out binary-boolean"> <?php echo TXT_OPTIONS_OUTPUT_7; ?>
-							<?php echo TXT_OPTIONS_OUTPUT_8; ?>		<input 	type="text" 		name="out_nomFichier" 	size="10" id="texte72"> </br>
+							<?php echo TXT_OPTIONS_OUTPUT_8; ?>		<input 	type="text" 		name="out_nomFichier" 	size="10" id="texte72"> <br/>
 							<input type="radio" 	name="choix7" 	value="out binary-doubles"> <?php echo TXT_OPTIONS_OUTPUT_9; ?>
-							<?php echo TXT_OPTIONS_OUTPUT_10; ?> 	<input 	type="text" 		name="out_nomFichier" 	size="10" id="texte73"> </br>	
+							<?php echo TXT_OPTIONS_OUTPUT_10; ?> 	<input 	type="text" 		name="out_nomFichier" 	size="10" id="texte73"> <br/>	
 							<input type="radio" 	name="choix7" 	value="out matlab"> 		<?php echo TXT_OPTIONS_OUTPUT_11; ?>
 							<?php echo TXT_OPTIONS_OUTPUT_12; ?> 	<input type="text" 			name="out_nomFichier" 	size="10" id="texte74">
 						</div>
 					</div>
 
-				</br></br>
+				<br/><br/>
 
 				<!-- PARAMETERS 1 -->
 					<div id="part">
@@ -174,8 +173,8 @@
 							<?php echo TXT_OPTIONS_EFM_LINE; ?> <input type="radio" name="choix8" value="rowordering"> 
 						</div>
 						<div id="buttons3-2">
-							<input type="radio" name="choix9" value="adjacency-method pattern-tree-minzero"> 	<?php echo TXT_OPTIONS_EFM_ADJA_1; ?>	</br>	
-							<input type="radio" name="choix9" value="adjacency-method pattern-tree-rank"> 		<?php echo TXT_OPTIONS_EFM_ADJA_2; ?> 	</br>	
+							<input type="radio" name="choix9" value="adjacency-method pattern-tree-minzero"> 	<?php echo TXT_OPTIONS_EFM_ADJA_1; ?>	<br/>	
+							<input type="radio" name="choix9" value="adjacency-method pattern-tree-rank"> 		<?php echo TXT_OPTIONS_EFM_ADJA_2; ?> 	<br/>	
 						</div>
 						<div id="buttons3-3">
 							<input type="radio" name="choix10" value="maxthreads"> <?php echo TXT_OPTIONS_EFM_THREADS_1; ?>				
@@ -203,22 +202,22 @@
 					</div>
 					<div id="buttons">
 						<div id="buttons4-1">
-							<input type="radio" name="choix11" value="arithmetic double"> 		<?php echo TXT_OPTIONS_EFM_ARITHM_1; ?> 	</br>	
-							<input type="radio" name="choix11" value="arithmetic fractional"> 	<?php echo TXT_OPTIONS_EFM_ARITHM_2; ?> 	</br>	
+							<input type="radio" name="choix11" value="arithmetic double"> 		<?php echo TXT_OPTIONS_EFM_ARITHM_1; ?> 	<br/>	
+							<input type="radio" name="choix11" value="arithmetic fractional"> 	<?php echo TXT_OPTIONS_EFM_ARITHM_2; ?> 	<br/>	
 						</div>
 						<div id="buttons4-2">
-							<input type="radio" name="choix12" value="normalize max"> 						<?php echo TXT_OPTIONS_EFM_NORMAL_1; ?> 		</br>	
-							<input type="radio" name="choix12" value="normalize min"> 						<?php echo TXT_OPTIONS_EFM_NORMAL_2; ?> 		</br>	
-							<input type="radio" name="choix12" value="normalize norm2"> 					<?php echo TXT_OPTIONS_EFM_NORMAL_3; ?> 		</br>	
-							<input type="radio" name="choix12" value="normalize squared"> 					<?php echo TXT_OPTIONS_EFM_NORMAL_4; ?> 		</br>	
-							<input type="radio" name="choix12" value="normalize none" checked="checked"> 	<?php echo TXT_OPTIONS_EFM_NORMAL_5; ?> 		</br>	
+							<input type="radio" name="choix12" value="normalize max"> 						<?php echo TXT_OPTIONS_EFM_NORMAL_1; ?> 		<br/>	
+							<input type="radio" name="choix12" value="normalize min"> 						<?php echo TXT_OPTIONS_EFM_NORMAL_2; ?> 		<br/>	
+							<input type="radio" name="choix12" value="normalize norm2"> 					<?php echo TXT_OPTIONS_EFM_NORMAL_3; ?> 		<br/>	
+							<input type="radio" name="choix12" value="normalize squared"> 					<?php echo TXT_OPTIONS_EFM_NORMAL_4; ?> 		<br/>	
+							<input type="radio" name="choix12" value="normalize none" checked="checked"> 	<?php echo TXT_OPTIONS_EFM_NORMAL_5; ?> 		<br/>	
 						</div>
 						<div id="buttons4-3">
 							<input type="radio" name="choix13" value="precision "> <?php echo TXT_OPTIONS_EFM_P_FRAC_1; ?>
 								<?php echo TXT_OPTIONS_EFM_VALUE; ?>	<input type="text" 		name="precisionThread" 	size="10" id="texte13">
 						</div>
 						<div id="buttons4-4">
-							<input type="radio" name="choix14" value="selftest true"> 	<?php echo TXT_OPTIONS_EFM_AUTO_T_1; ?> </br>	
+							<input type="radio" name="choix14" value="selftest true"> 	<?php echo TXT_OPTIONS_EFM_AUTO_T_1; ?> <br/>	
 							<input type="radio" name="choix14" value="selftest false"> 	<?php echo TXT_OPTIONS_EFM_AUTO_T_2; ?>
 						</div>
 					</div>
@@ -235,7 +234,7 @@
 			$_SESSION['commande']=$commande;
 			echo $commande;
 		?>
-	</br>
+	<br/>
 		<a href='results.php'> Lancement </a>
 	</body>
 </html>
