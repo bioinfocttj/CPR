2c8820ced7ec6d40728613103962d2a3e65b2005
Charlotte Herice <cherice@emi.u-bordeaux1.fr>
2013-02-13 10:25:20 +0100

    load ok

--- WRET/load.php
+++ WRET/load.php
@@ -36,60 +36,6 @@
 	</div>
 	
 	<body>
-		<div id="loadBody"> 
-
-			<h1><?php echo TXT_LOAD_TITLE; ?></h1>
-			<h4><?php echo TXT_LOAD_INSTRUCTION_1; ?></h4>
-
-			<table border="3" width=30%>
-				<form action="<?php move_uploaded_file($_FILES['sfile']['tmp_name'],$_FILES['sfile']['name']);?>" enctype="multipart/form-data" method="post">
-				<tr><td>
-				<label ="NomDeFichier">
-				<?php echo TXT_LOAD_FILE_NAME; ?><br />
-				</label>
-				<input type="file" name="sfile" id="sfile" /> 	<?php echo TXT_LOAD_SFILE; ?> 
-				<input type="file" name="mfile" id="mfile" /> 	<?php echo TXT_LOAD_MFILE; ?> 
-				<input type="file" name="rvfile" id="rvfile" /> <?php echo TXT_LOAD_RVFILE; ?> 
-				<input type="file" name="grfile" id="grfile" /> <?php echo TXT_LOAD_GRFILE; ?> 
-				<input type="file" name="rfile" id="rfile" /> 	<?php echo TXT_LOAD_RFILE; ?> 
-				</td></tr><tr><td>
-				<input type="submit" name="submit" value="Recuperer" /><!DOCTYPE html >
-<?php require("languages/choosen_languages.php");?>
-<html>
-	<head>
-		<title><?php echo TXT_OPTIONS_SITE_TITLE; ?></title>
-		<link rel="stylesheet" media="screen" type="text/css" href="style.css"/>
-		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-		<script  type="text/javascript" src="actionLoad.js">	</script>
-	</head>
-
-	<body>
-		<?php 
-			$protocol = strpos(strtolower($_SERVER['SERVER_PROTOCOL']),'https') === FALSE ? 'http' : 'https';
-			$host = $_SERVER['HTTP_HOST'];
-			$script = $_SERVER['SCRIPT_NAME'];
-			if(!isset($params)){
-				$currentUrl = $protocol . '://' . $host . $script . '?';
-			}
-			else{
-				$currentUrl = $protocol . '://' . $host . $script . '?' . $params;
-			}
-			$en=$currentUrl."lang=en";
-			$fr=$currentUrl."lang=fr";
-			$de=$currentUrl."lang=de";
-		?>
-		<div id="menu">
-			<ul>
-				<li><a href="index.php">		<?php echo TXT_MENU_HOME; ?>	</a></li>
-				<li><a href="create.php">		<?php echo TXT_MENU_CREATE; ?> 	</a></li>
-				<li><a href="load.php">			<?php echo TXT_MENU_LOAD; ?>	</a></li>
-				<li><a href="modify.php">		<?php echo TXT_MENU_MODIFY; ?>	</a></li>
-				<li><a href="help.php">			<?php echo TXT_MENU_HELP; ?>	</a></li>
-				<li><a href=<?php echo $en?>><img src="Images/English-Language-Flag-3-icon.png" alt="english_flag.png"></a></li>
-				<li><a href=<?php echo $fr?>><img src="Images/French-Flag.png" alt="french_flag.png"></a></li>
-				<li><a href=<?php echo $de?>><img src="Images/german_flag.gif" alt="german_flag.gif"></a></li>
-			</ul>
-		</div>
 
 		<div id="loadBody">
 			<h1><?php echo TXT_LOAD_TITLE; ?></h1>
@@ -163,7 +109,7 @@
 								<input type="file" name="mfile" 	id="meta" /> 		<?php echo TXT_LOAD_MFILE; ?> 
 								<input type="file" name="rvfile" 	id="rev" /> 		<?php echo TXT_LOAD_RVFILE; ?> 
 								<input type="file" name="grfile" 	id="generule" /> 	<?php echo TXT_LOAD_GRFILE; ?> 
-								<input type="file" name="rfile" 	id="reac" /> 		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo TXT_LOAD_RFILE; ?> 
+								<input type="file" name="rfile" 	id="reac" /> 		&nbsp;&nbsp;<?php echo TXT_LOAD_RFILE; ?> 
 								</td></tr><tr><td>
 								</td></tr>
 							</table>
